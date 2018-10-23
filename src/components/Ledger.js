36b36638c02b19bad014642818fe3e896c72f7a2
David Jungst <djungst@gmail.com>
2018-10-23 11:19:27 -0600

    remove old pre-react code

--- src/components/Ledger.js
+++ src/components/Ledger.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import Transactions from './Transactions'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  flex: 2
+`
 
 const Ledger = () => (
-  <div>
+  <Container>
     <Transactions />
-  </div>
+  </Container>
 )
 export default Ledger
 
