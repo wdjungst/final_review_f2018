0f98bc541ab00579f217a46bd884cda1f40e058d
David Jungst <djungst@gmail.com>
2018-10-24 12:32:08 -0600

    lazy load routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom'
 import { ProtectedRoute } from '@devpoint/dps-react-kit'
-import Home from './Home'
-import Auth from './Auth'
-import FetchUser from './FetchUser'
-import Contact from './Contact'
+import Loadable from 'react-loadable'
+//import Home from './Home'
+//import Auth from './Auth'
+//import FetchUser from './FetchUser'
+//import Contact from './Contact'
+
+const Loading = () => <div>Loading...</div>
+
+const Home = Loadable({
+  loader: () => import('./Home'),
+  loading: Loading
+})
+
+const Auth = Loadable({
+  loader: () => import('./Auth'),
+  loading: Loading,
+})
+
+const Contact= Loadable({
+  loader: () => import('./Contact'),
+  loading: Loading,
+})
 
 const App = () => (
   <>
